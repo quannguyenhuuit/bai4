14f2dcaae05e11f61c89e7be04b90e451a71bea1
Van Nhan <nguyenvannhan0810@gmail.com>
2017-07-05 00:21:43 +0700

    Initial Commit

---
+++ TableView_BaiMauMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView_BaiMauMVC
+//
+//  Created by Nguyễn Văn Nhàn on 7/5/17.
+//  Copyright © 2017 Nguyễn Văn Nhàn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
